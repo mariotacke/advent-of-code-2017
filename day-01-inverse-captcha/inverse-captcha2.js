e12d34723543078b20bddf1c233f85f7973dddba
Mario Tacke <tacke.mario@gmail.com>
2017-12-01 20:05:06 -0800

    feature: Inverse Captcha (part 2)

---
+++ day-01-inverse-captcha/inverse-captcha2.js
@@ -0,0 +1,24 @@
+const inverseCaptcha = (input) => {
+  return input
+    .split('')
+    .map((x) => parseInt(x))
+    .reduce((accumulator, currentValue, currentIndex, array) => {
+      const half = array.length / 2;
+      
+      if (currentIndex + half <= array.length - 1) {
+        if (currentValue === array[currentIndex + half]) {
+          return accumulator + currentValue;
+        }
+      } else {
+        const skip = currentIndex + half - array.length;
+        
+        if (currentValue === array[skip]) {
+          return accumulator + currentValue;
+        }
+      }
+
+      return accumulator;
+    }, 0);
+};
+
+module.exports = inverseCaptcha;
--- day-01-inverse-captcha/test.js
+++ day-01-inverse-captcha/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const inverseCaptcha = require('./inverse-captcha');
+const inverseCaptcha2 = require('./inverse-captcha2');
 
 describe('Inverse Captcha', () => {
   it('should properly calculate 1122', () => {
@@ -17,5 +18,27 @@ describe('Inverse Captcha', () => {
   
   it('should properly calculate 91212129', () => {    
     assert.equal(9, inverseCaptcha('91212129'));
+  });
+  
+  describe('Part 2', () => {
+    it('should properly calculate 1212', () => {
+      assert.equal(6, inverseCaptcha2('1212'));
+    });
+    
+    it('should properly calculate 1221', () => {
+      assert.equal(0, inverseCaptcha2('1221'));
+    });
+    
+    it('should properly calculate 123425', () => {
+      assert.equal(4, inverseCaptcha2('123425'));
+    });
+    
+    it('should properly calculate 123123', () => {
+      assert.equal(12, inverseCaptcha2('123123'));
+    });
+    
+    it('should properly calculate 12131415', () => {
+      assert.equal(4, inverseCaptcha2('12131415'));
+    });
   });
 });
