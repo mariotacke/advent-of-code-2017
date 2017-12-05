72753bddda5b9e8de76ed2bcf0154db7618c0a92
Mario Tacke <tacke.mario@gmail.com>
2017-12-04 21:54:49 -0800

    feature: Twisty Trampolines (part 2)

--- day-05-a-maze-of-twisty-trampolines/test.js
+++ day-05-a-maze-of-twisty-trampolines/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const twistyTrampolines = require('./twisty-trampolines');
+const twistyTrampolines2 = require('./twisty-trampolines2');
 
 describe('Day 5: A Maze of Twisty Trampolines, All Alike', () => {
   it('should properly calculate steps', () => {
@@ -12,5 +13,18 @@ describe('Day 5: A Maze of Twisty Trampolines, All Alike', () => {
       -3`;
     
     assert.equal(5, twistyTrampolines(offsets));
+  });
+  
+  describe('Part Two', () => {
+    it('should properly calculate other steps', () => {
+      const offsets = 
+        `0
+         3
+         0
+         1
+        -3`;
+      
+      assert.equal(10, twistyTrampolines2(offsets));
+    });
   });
 });
