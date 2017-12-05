97a7211090519c2474cb3363083524d30524a813
Mario Tacke <tacke.mario@gmail.com>
2017-12-04 20:12:04 -0800

    feature: High-Entropy Passphrases (part 2)

---
+++ day-04-high-entropy-passphrases/high-entropy-passphrases2.js
@@ -0,0 +1,16 @@
+const highEntropyPassphrases = (input) => {
+  return input
+    .split('\n')
+    .map((x) => x
+      .trim()
+      .split(/[\s\t/]+/)
+      .map((w) => w.split('').sort().join())
+    )
+    .reduce((accumulator, currentValue, currentIndex, array) => {
+      const uniqueWords = [...new Set(currentValue)];
+      
+      return accumulator + (uniqueWords.length === currentValue.length ? 1 : 0);
+    }, 0);
+};
+
+module.exports = highEntropyPassphrases;
--- day-04-high-entropy-passphrases/test.js
+++ day-04-high-entropy-passphrases/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const highEntropyPassphrases = require('./high-entropy-passphrases');
+const highEntropyPassphrases2 = require('./high-entropy-passphrases2');
 
 describe('Day 4: High-Entropy Passphrases', () => {
   it('should properly evaluate passphrases', () => {
@@ -10,5 +11,18 @@ describe('Day 4: High-Entropy Passphrases', () => {
        aa bb cc dd aaa`;
     
     assert.equal(2, highEntropyPassphrases(passphrases));
+  });
+  
+  describe('Part Two', () => {
+    it('should properly evaluate other passphrases', () => {
+      const passphrases = 
+        `abcde fghij
+         abcde xyz ecdab
+         a ab abc abd abf abj
+         iiii oiii ooii oooi oooo
+         oiii ioii iioi iiio`;
+      
+      assert.equal(3, highEntropyPassphrases2(passphrases));
+    });
   });
 });
