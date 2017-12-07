077b75d0a845d50abcb857d24fc8f1ec201b7428
Mario Tacke <tacke.mario@gmail.com>
2017-12-06 20:12:48 -0800

    feature: Memory Reallocation (part 2)

---
+++ day-06-memory-reallocation/memory-reallocation2.js
@@ -0,0 +1,49 @@
+const memoryReallocation = (input) => {
+  const banks = input.trim().split(/[\s\t/]+/).map((x) => parseInt(x));
+
+  const patterns = {};
+  
+  let snapshot = banks.join(':');
+
+  patterns[snapshot] = {
+    lastSeen: 0,
+    alreadySeen: false,
+  };
+
+  while (!(patterns[snapshot] || {}).alreadySeen) {
+    let blocks = Math.max(...banks);
+    let currentIndex = banks.findIndex((x) => x === blocks);
+    
+    if (!patterns[snapshot]) {
+      patterns[snapshot] = {
+        lastSeen: 0,
+        alreadySeen: false,
+      }
+    }
+    
+    banks[currentIndex] = 0;
+    currentIndex += 1;
+    
+    while (blocks > 0) {
+      if (currentIndex === banks.length) {
+        currentIndex = 0;
+      }
+      
+      banks[currentIndex] += 1;
+      currentIndex += 1;
+      blocks -= 1;
+    }
+
+    snapshot = banks.join(':');
+    
+    if (patterns[snapshot]) {
+      return patterns[snapshot].lastSeen + 1;
+    }
+    
+    Object
+      .keys(patterns)
+      .forEach((x) => patterns[x].lastSeen += 1);
+  }
+};
+
+module.exports = memoryReallocation;
--- day-06-memory-reallocation/test.js
+++ day-06-memory-reallocation/test.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
 
 const memoryReallocation = require('./memory-reallocation');
+const memoryReallocation2 = require('./memory-reallocation2');
 
 describe('Day 6: Memory Reallocation', () => {
   it('should properly calculate steps', () => {
     const banks = '0 2 7 0';
     
-    assert.equal(5, memoryReallocation(banks));
+    assert.equal(memoryReallocation(banks), 5);
+  });
+  
+  describe('Part Two', () => {
+    it('should properly calculate steps', () => {
+      const banks = '0 2 7 0';
+      
+      assert.equal(memoryReallocation2(banks), 4);
+    });
   });
 });
