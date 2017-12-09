6ee7faefa34a09af2f8b56e20426a6909644412c
Mario Tacke <tacke.mario@gmail.com>
2017-12-08 23:01:25 -0800

    feature: Recursive Circus (part 2)

---
+++ day-07-recursive-circus/recursive-circus2.js
@@ -0,0 +1,71 @@
+const recursiveCircus = (input) => {
+  const nodes = {};
+  
+  const instructions = input
+    .split('\n')
+    .map((x) => x.trim());
+    
+  for (let i = 0; i < instructions.length; i++) {
+    const match = instructions[i].match(/^([\w]+)\s\((\d+)\)(?:\s->\s)?(.*)$/i);
+
+    const node = {
+      name: match[1],
+      weight: parseInt(match[2]),
+      parent: null,
+      children: match[3] ? match[3].split(', ') : [],
+    };
+    
+    nodes[node.name] = node;
+  }  
+
+  const keys = Object.keys(nodes);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    
+    for (let j = 0; j < nodes[key].children.length; j++) {
+      const child = nodes[key].children[j];
+      
+      nodes[key].children[j] = nodes[child];
+      nodes[child].parent = nodes[key];
+    }
+  }
+  
+  const weigh = (node) => {
+    return node.weight + node.children
+      .map(weigh)
+      .reduce((sum, current) => sum + current, 0);
+  };
+  
+  let answer = 0;
+  
+  const traverse = (node, difference = 0) => {
+    const children = node.children.map((child) => {
+      return {
+        name: child.name,
+        weight: weigh(child)
+      };
+    });
+    
+    const unbalancedChild = children.find((child) => {
+      return children.filter((x) => x.weight === child.weight).length === 1;
+    });
+    
+    if (!unbalancedChild) {
+      answer = nodes[node.name].weight - difference;
+    } else {
+      const balancedChild = children
+        .find((child) => children.filter((x) => x.weight === child.weight).length > 1);
+      
+      traverse(nodes[unbalancedChild.name], unbalancedChild.weight - balancedChild.weight);
+    }
+  };
+  
+  const rootNode = Object.values(nodes).find((node) => node.parent === null);
+  
+  traverse(rootNode);
+  
+  return answer;
+};
+
+module.exports = recursiveCircus;
--- day-07-recursive-circus/test.js
+++ day-07-recursive-circus/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const recursiveCircus = require('./recursive-circus');
+const recursiveCircus2 = require('./recursive-circus2');
 
 describe('Day 7: Recursive Circus', () => {
   it('should properly determine bottom program', () => {
@@ -20,5 +21,26 @@ describe('Day 7: Recursive Circus', () => {
        cntj (57)`;
     
     assert.equal(recursiveCircus(instructions), 'tknk');
+  });
+  
+  describe('Part Two', () => {
+    it('should properly determine proper weight to balance', () => {
+      const instructions =
+        `pbga (66)
+         xhth (57)
+         ebii (61)
+         havc (66)
+         ktlj (57)
+         fwft (72) -> ktlj, cntj, xhth
+         qoyq (66)
+         padx (45) -> pbga, havc, qoyq
+         tknk (41) -> ugml, padx, fwft
+         jptl (61)
+         ugml (68) -> gyxo, ebii, jptl
+         gyxo (61)
+         cntj (57)`;
+      
+      assert.equal(recursiveCircus2(instructions), 60);
+    });
   });
 });
