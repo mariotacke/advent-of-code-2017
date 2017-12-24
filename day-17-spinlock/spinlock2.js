d052377b1e5f173a4cb4fa3606e22813b98c186b
Mario Tacke <tacke.mario@gmail.com>
2017-12-23 18:39:48 -0800

    perf: Spinlock (part 2)

--- day-17-spinlock/spinlock2.js
+++ day-17-spinlock/spinlock2.js
@@ -1,17 +1,9 @@
-const getNextPosition = (iteration, steps, position) => {
-  for (let i = 0; i < steps; i++) {
-    position = position + 1 >= iteration ? 0 : position + 1;
-  }
-
-  return position;
-};
-
 const spinlock = (steps, iterations = 50000000) => {
   let value = undefined;
   let position = 0;
 
   for (let i = 1; i < iterations; i++) {
-    const next = getNextPosition(i, steps, position);
+    const next = (position + steps) % i;
 
     if (next === 0) {
       value = i;
