53a2202c248d9656e1e6a06d9d881576f6786724
Mario Tacke <tacke.mario@gmail.com>
2018-02-21 14:25:49 -0800

    feature: A Series of Tubes (part 2)

--- day-19-a-series-of-tubes/test.js
+++ day-19-a-series-of-tubes/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const tubes = require('./tubes');
+const tubes2 = require('./tubes2');
 
 describe('Day 19: A Series of Tubes', () => {
   it('should calculate tube path', () => {
@@ -17,5 +18,23 @@ describe('Day 19: A Series of Tubes', () => {
     const path = tubes(instructions);
 
     assert.equal(path, 'ABCDEF');
+  });
+
+  describe('Part 2', () => {
+    it('should calculate tube path', () => {
+      const instructions =
+        `
+        |
+        |  +--+
+        A  |  C
+    F---|----E|--+
+        |  |  |  D
+        +B-+  +--+
+        `;
+
+      const steps = tubes2(instructions);
+
+      assert.equal(steps, 38);
+    });
   });
 });
---
+++ day-19-a-series-of-tubes/tubes2.js
@@ -0,0 +1,56 @@
+const tubes = (input) => {
+  const width = Math.max(...input.split('\n').map((line) => line.length));
+  const maze = input
+    .split('\n')
+    .filter((line) => line.trim().length)
+    .map((line) => line
+      .padEnd(width)
+      .split('')
+      .map((x) => x.trim())
+    );
+
+  const cursor = {
+    x: maze[0].findIndex((x) => x === '|'),
+    y: 0,
+    direction: {
+      x: 0,
+      y: 1,
+    },
+  };
+
+  let steps = 0;
+
+  while (maze[cursor.y][cursor.x]) {
+    cursor.x += cursor.direction.x;
+    cursor.y += cursor.direction.y;
+    steps++;
+
+    if (maze[cursor.y][cursor.x] === '|' || maze[cursor.y][cursor.x] === '-') {
+      continue;
+    }
+
+    if (maze[cursor.y][cursor.x] === '+') {
+      if (Math.abs(cursor.direction.y)) {
+        if (maze[cursor.y][cursor.x - 1]) {
+          cursor.direction.x = -1;
+        } else if (maze[cursor.y][cursor.x + 1]) {
+          cursor.direction.x = 1;
+        }
+
+        cursor.direction.y = 0;
+      } else {
+        if (maze[cursor.y - 1][cursor.x]) {
+          cursor.direction.y = -1;
+        } else if (maze[cursor.y + 1][cursor.x]) {
+          cursor.direction.y = 1;
+        }
+
+        cursor.direction.x = 0;
+      }
+    }
+  }
+
+  return steps;
+};
+
+module.exports = tubes;
